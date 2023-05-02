319b831f4ef4840af8f258f10b0ac4ac4d7df9f7
easherma <ericandrewsherman@gmail.com>
2023-05-02 15:32:01 -0500

    add new data and chicago community areas

--- aggregates.js
+++ aggregates.js
@@ -1,8 +1,10 @@
-async function createChoroplethLayer(
-  dataCsvUrl,
-  dataToDisplay = [],
-  pickedColor = `27,120,55`
-) {
+async function createChoroplethLayer(options) {
+  const {
+    dataCsvUrl,
+    dataToDisplay = [],
+    pickedColor = `27,120,55`,
+    allCols = true,
+  } = options;
   const filteredGeoJSON = await mergeDataWithZipcodes(dataCsvUrl);
   // Aggregate csv data by zip code
   const countByZipcode = {};
@@ -24,51 +26,61 @@ async function createChoroplethLayer(
     style: (feature) => {
       const zipcode = feature.properties.ZCTA5CE20;
       const count = dataByZipcode[zipcode] ? dataByZipcode[zipcode].length : 0;
-      const color = `rgba(${pickedColor}, ${count / 2})`;
-      return { fillColor: color, fillOpacity: 0.9, weight: 1, color: color };
+      const color = `rgba(${pickedColor}, ${count / 3})`;
+      return { fillColor: color, fillOpacity: 0.9, weight: 1, opacity: 0.9, color: '#242f00' };
     },
     onEachFeature: (feature, layer) => {
       const zipcode = feature.properties.ZCTA5CE20;
       const count = dataByZipcode[zipcode] ? dataByZipcode[zipcode].length : 0;
       const data = dataByZipcode[zipcode] || [];
 
-      // Determine which columns to display based on the `options` object
-      const columnsToDisplay = dataToDisplay ? dataToDisplay || [] : [];
+      createTable();
 
-      // If no columns are specified, display all columns
-      const keys =
-        columnsToDisplay.length > 0
-          ? columnsToDisplay
-          : Object.keys(data[0] || {});
+      function createTable() {
+        // Determine which columns to display based on the object
+        const columnsToDisplay = dataToDisplay ? dataToDisplay || [] : [];
 
-      // Create the table headers
-      const headers = keys.map((key) => `<th>${key}</th>`).join("");
+        // If no columns are specified, display all columns
+        const keys =
+          columnsToDisplay.length > 0
+            ? columnsToDisplay
+            : Object.keys(data[0] || {});
 
-      // Create the table rows
-      const rows = data
-        .map((row) => {
-          // Create a table cell for each value in the row
-          const cells = keys.map((key) => `<td>${row[key]}</td>`).join("");
-          return `<tr>${cells}</tr>`;
-        })
-        .join("");
+        // Create the table headers
+        const headers = keys.map((key) => `<th>${key}</th>`).join("");
 
-      // Create the final popup content with the table
-      const popupContent = `
-          <div style="overflow:auto;">
+        // Create the table rows
+        const rows = data
+          .map((row) => {
+            // Create a table cell for each value in the row
+            const cells = keys.map((key) => `<td>${row[key]}</td>`).join("");
+            return `<tr>${cells}</tr>`;
+          })
+          .join("");
+
+        const table = `
+        <table>
+          <thead>
+            <tr>${headers}</tr>
+          </thead>
+          <tbody>
+            ${rows}
+          </tbody>
+        </table>`;
+
+        // Create the final popup content with the table
+        const summaryContent = `
             <div><strong>Zip Code:</strong> ${zipcode}</div>
             <div><strong>Count:</strong> ${count}</div>
-            <table>
-              <thead>
-                <tr>${headers}</tr>
-              </thead>
-              <tbody>
-                ${rows}
-              </tbody>
-            </table>
-          </div>
         `;
-      layer.bindPopup(popupContent);
+        if (allCols) {
+          const popupContent = `<div style="overflow:auto;">${summaryContent} ${table}</div>`;
+          layer.bindPopup(popupContent);
+        } else {
+          const popupContent = `${summaryContent}`;
+          layer.bindPopup(popupContent);
+        }
+      }
     },
   });
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 async function getData(dataCsvUrl) {
   const zipcodeGeoJSON = await fetchZipcodeData();
-
   const CsvData = await fetch(dataCsvUrl);
   const dataCsv = await CsvData.text();
   const config = {
@@ -17,6 +16,8 @@ function fixHeader(header) {
   const transformedHeader = header.replace(/\s+/g, "_");
   if (transformedHeader === "zip_code") {
     return "zipcode";
+  } else if (transformedHeader === "zip_code") {
+    return "zipcode";
   }
   return transformedHeader;
 }
@@ -27,9 +28,25 @@ async function fetchZipcodeData() {
   return data;
 }
 
+async function fetchCommAreas() {
+  const response = await fetch("chi_comm_areas.geojson");
+  const data = await response.json();
+  return data;
+}
+
 async function init() {
   // Create the map and set its initial view to center over Illinois
   const map = L.map("map").setView([40.1, -89.3], 8);
+
+  const CartoDB_Positron = L.tileLayer(
+    "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: "abcd",
+      maxZoom: 18,
+    }
+  ).addTo(map);
   // add the base layer
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
@@ -37,30 +54,54 @@ async function init() {
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-  }).addTo(map);
+  });
   // Fetch data
   // Define the URLs for the GeoJSON and CSV files
   const zipcodeGeoJSONUrl = "./zips_near_illinois.geojson";
   const dataCsvUrl = "Help_Desk_Tickets.csv";
   const filteredGeoJSON = await mergeDataWithZipcodes(dataCsvUrl);
-  
+
   // Create choropleth layer
-  const helpDeskCounts = await createChoroplethLayer(dataCsvUrl);
-  const OutreachCounts = await createChoroplethLayer(
-    "Door_And_Phone_Outreach.csv",
-    ["client_name"],
-    (pickedColor = `118,42,131`)
-  );
-  const data = createLayer(filteredGeoJSON, map);
+  const helpDeskCounts = await createChoroplethLayer({
+    dataCsvUrl: "helpdesk.csv",
+  });
+  const OutreachCounts = await createChoroplethLayer({
+    dataCsvUrl: "outreach.csv",
+    dataToDisplay: ["client_name"],
+    pickedColor: `118,42,131`.y,
+  });
+  const Applicants = await createChoroplethLayer({
+    dataCsvUrl: "DCEO.csv",
+    allCols: false,
+  });
+  // const data = createLayer(filteredGeoJSON, map);
+
+  const CommAreas = await createCommAreas();
 
   const layers = {
     "Help Desk Counts": helpDeskCounts,
     "Targeted Outreach": OutreachCounts,
+    Applicants: Applicants,
+    "Chicago Community Areas": CommAreas,
   };
   layerControl = L.control
     .layers(null, layers, { collapsed: false })
     .addTo(map);
-  helpDeskCounts.addTo(map);
+
+  async function createCommAreas() {
+    const areas = await fetchCommAreas();
+    const myStyle = {
+      color: "#f8ac51",
+      weight: 4,
+      opacity: 1,
+      fillOpacity: 0,
+      fillcolor: "#fff",
+    };
+    const layer = L.geoJSON(areas, {
+      style: myStyle,
+    });
+    return layer;
+  }
 }
 
 init();
