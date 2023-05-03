1ef697991fcc122e68f92adc7ef13e26842e884c
Eric Sherman <esherman@truthinitiative.org>
2023-05-02 19:12:38 -0500

    add applications layer

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function fetchCommAreas() {
   return data;
 }
 
+async function fetchAppCounts() {
+  const response = await fetch("applications_counts.geojson");
+  const data = await response.json();
+  return data;
+}
+
 async function init() {
   // Create the map and set its initial view to center over Illinois
   const map = L.map("map").setView([40.1, -89.3], 8);
@@ -70,10 +76,7 @@ async function init() {
     dataToDisplay: ["client_name"],
     pickedColor: `118,42,131`.y,
   });
-  const Applicants = await createChoroplethLayer({
-    dataCsvUrl: "DCEO.csv",
-    allCols: false,
-  });
+  const Applicants = await createAppCounts();
   // const data = createLayer(filteredGeoJSON, map);
 
   const CommAreas = await createCommAreas();
@@ -99,6 +102,148 @@ async function init() {
     };
     const layer = L.geoJSON(areas, {
       style: myStyle,
+    });
+    return layer;
+  }
+
+  async function createAppCounts() {
+    const areas = await fetchAppCounts();
+
+    function onEachFeature(feature, layer) {
+      const zipcode = feature.properties.ZCTA5CE20;
+      const count = feature.properties.count;
+      // Create the final popup content with the table
+      const summaryContent = `
+      <div><strong>Zip Code:</strong> ${zipcode}</div>
+      <div><strong>Count:</strong> ${count}</div>
+  `;
+      const popupContent = `${summaryContent}`;
+      layer.bindPopup(popupContent);
+    }
+
+    const layer = L.geoJSON(areas, {
+      onEachFeature: onEachFeature,
+      style: function (feature) {
+        if (
+          feature.properties["count"] >= 1.0 &&
+          feature.properties["count"] <= 1.78117
+        ) {
+          return {
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(247,252,245,1.0)",
+            interactive: true,
+          };
+        }
+        if (
+          feature.properties["count"] >= 1.78117 &&
+          feature.properties["count"] <= 3.498728
+        ) {
+          return {
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(221,242,215,1.0)",
+            interactive: true,
+          };
+        }
+        if (
+          feature.properties["count"] >= 3.498728 &&
+          feature.properties["count"] <= 9.160305
+        ) {
+          return {
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(178,224,171,1.0)",
+            interactive: true,
+          };
+        }
+        if (
+          feature.properties["count"] >= 9.160305 &&
+          feature.properties["count"] <= 15.298982
+        ) {
+          return {
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(123,199,124,1.0)",
+            interactive: true,
+          };
+        }
+        if (
+          feature.properties["count"] >= 15.298982 &&
+          feature.properties["count"] <= 19.497455
+        ) {
+          return {
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(61,167,90,1.0)",
+            interactive: true,
+          };
+        }
+        if (
+          feature.properties["count"] >= 19.497455 &&
+          feature.properties["count"] <= 28.816794
+        ) {
+          return {
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(19,126,58,1.0)",
+            interactive: true,
+          };
+        }
+        if (
+          feature.properties["count"] >= 28.816794 &&
+          feature.properties["count"] <= 50.0
+        ) {
+          return {
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(0,68,27,1.0)",
+            interactive: true,
+          };
+        }
+      },
     });
     return layer;
   }
